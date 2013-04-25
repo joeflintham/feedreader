d862179cf5904a736f38a5b7f86f760ac1bfc3f4
Joe Flintham <joeflintham@gmail.com>
2013-04-25 16:56:20 +0100

    Version 0.1

--- loadFeed.js
+++ loadFeed.js
@@ -1,55 +1,81 @@
 
 
 /* Load the news listing */
-function loadFeeds(feedUrl) {
+function loadFeeds(feedData) {
+    if ((!feedData && feedData.feedUrl)) return false;
+    var feedUrl = feedData.feedUrl;
+    var defaultImageUrl = feedData.defaultImageUrl;
+    var defaultImageWidth = (feedData.defaultImageWidth) ? feedData.defaultImageWidth : 153;
+    var defaultImageHeight = (feedData.defaultImageHeight) ? feedData.defaultImageHeight : 102;
+    var defaultFeedWidth = (feedData.defaultFeedWidth) ? feedData.defaultFeedWidth : 406;
+    var newsTextWidth = defaultFeedWidth - defaultImageWidth - 14; 
+    var feedContainerID = (feedData.feedContainerID) ? feedData.feedContainerID : ''
+ 
 	//feedUrl += '?nocache='+(new Date).getTime();	// uncomment this line if cache causes unreasonable delays
-	var feed = new google.feeds.Feed(feedUrl);
-	feed.setNumEntries(10);
-	feed.load(function(result) {
-		if (!result.error) {
-			var container = document.getElementById("news-feed");
-			for (var i = 0; i < result.feed.entries.length; i++) {
-				var entry = result.feed.entries[i];
-				var newsBlockEl = document.createElement("div");
-				var oddEvenClass = (i % 2) ? "table_even" : "table_odd";
-				newsBlockEl.setAttribute("class", "body_newsarticle "+oddEvenClass);
+    var feed = new google.feeds.Feed(feedUrl);
+    feed.setNumEntries(10);
+    feed.load(function(result) {
+	console.log(defaultFeedWidth)
+	if (!result.error) {
+	    var container = document.getElementById(feedContainerID);
+	    if (!container) {
+		container = document.createElement("div");
+		document.body.appendChild(container);
+	    }
+	    container.style.width = defaultFeedWidth + "px";
+	    for (var i = 0; i < result.feed.entries.length; i++) {
+		var entry = result.feed.entries[i];
+		var newsBlockEl = document.createElement("div");
+		var oddEvenClass = (i % 2) ? "table_even" : "table_odd";
+		newsBlockEl.setAttribute("class", "body_newsarticle "+oddEvenClass);
+		newsBlockText = document.createElement("div");
+		newsBlockText.setAttribute("class", "body_newstextarea");
+		newsBlockText.style.width = newsTextWidth + "px";
+		
+		// Set up title and content elements
+		
+		var titleEl = document.createElement("a");
+		var bodyEl = document.createElement("p");
+		var dateEl = document.createElement("span");
 				
-				// Set up title and content elements
-				var titleEl = document.createElement("a");
-				var bodyEl = document.createElement("p");
-				var dateEl = document.createElement("span");
+			// Set up title element
+			titleEl.setAttribute("href", entry.link);
+			titleEl.setAttribute("title", entry.title);
+			titleEl.setAttribute("class", "body_newsarticle_title");
+			titleEl.appendChild(document.createTextNode(entry.title));
+			newsBlockText.appendChild(titleEl);
 				
-				// Set up title element
-				titleEl.setAttribute("href", entry.link);
-				titleEl.setAttribute("title", entry.title);
-				titleEl.setAttribute("class", "body_newsarticle_title");
-				titleEl.appendChild(document.createTextNode(entry.title));
-				newsBlockEl.appendChild(titleEl);
+			// Set up date element
+			dateEl.setAttribute("class", "body_smalltext");
+			dateEl.appendChild(document.createTextNode(formatDate(new Date(entry.publishedDate))));
+			newsBlockText.appendChild(dateEl);
 				
-				// Set up date element
-				dateEl.setAttribute("class", "body_smalltext");
-				dateEl.appendChild(document.createTextNode(formatDate(new Date(entry.publishedDate))));
-				newsBlockEl.appendChild(dateEl);
-				
-				// Set up content element
-				bodyEl.setAttribute("class", "body_featurebox2_text");
-				bodyEl.innerHTML = entry.content;
-				var imgEls = bodyEl.getElementsByTagName("IMG");
-				if (imgEls.length == 0) {
-					imgEls[0] = document.createElement("img");
-					imgEls[0].setAttribute("width", "153");
-					imgEls[0].setAttribute("height", "102");
-					imgEls[0].setAttribute("alt", entry.title);
-					imgEls[0].setAttribute("src", "http://assets.bournemouth.ac.uk/aqua-retrofit/images/news-default-small.jpg");
-				}
-				newsBlockEl.insertBefore(imgEls[0],titleEl);
-				newsBlockEl.appendChild(bodyEl);
-				newsBlockEl.innerHTML += "<br class='clear' />";
-				
-				container.appendChild(newsBlockEl);
+			// Set up content element
+			bodyEl.setAttribute("class", "body_featurebox2_text");
+			bodyEl.innerHTML = entry.content;
+
+			var imgEls = bodyEl.getElementsByTagName("IMG");
+			if (imgEls.length == 0 && defaultImageUrl != '') {
+			    imgEls = [];
+			    imgEls[0] = document.createElement("img");
+			    imgEls[0].setAttribute("src", defaultImageUrl);
 			}
+			if (imgEls.length > 0) { 
+			    imgEls[0].setAttribute("width", defaultImageWidth);
+			    imgEls[0].setAttribute("height", defaultImageHeight);
+			    imgEls[0].setAttribute("alt", entry.title);
+			    newsBlockEl.appendChild(imgEls[0]); 
+			}
+
+
+
+			newsBlockEl.appendChild(newsBlockText);
+			newsBlockText.appendChild(bodyEl);
+			newsBlockEl.innerHTML += "<br class='clear' />";
+		        container.appendChild(newsBlockEl);
 		}
-	});	
+	}
+});	
 }
 
 /* Load the index page news list */
@@ -73,7 +99,7 @@ function loadIndexFeeds(feedUrl, numEntries) {
 				titleEl.appendChild(document.createTextNode(entry.title));
 				newsBlockEl.appendChild(titleEl);
 				
-				container.appendChild(newsBlockEl);
+				container.appendChild(newsBlockText);
 			}
 		}
 	});	
